253e2863357ec95f9de1d18a1dd562438646df45
Noussair <aouadinoussair@gmail.com>
2024-08-25 19:39:34 +0200

    views Detail && Liste and afficher data_base

--- event/views.py
+++ event/views.py
@@ -1,9 +1,17 @@
 from django.shortcuts import render
+from .models import Event
 
 
 # Create your views here.
 def event_list(request):
-    pass
+    event_list = Event.objects.all()
+    context={'events': event_list}
+    return render(request,'event/event_list.html',context)
+
+
+
 
 def event_detail(request , id):
-    pass
+    event_detail = Event.objects.get(id=id)
+    context={'event': event_detail}
+    return render(request,'event/event_detail.html',context)
